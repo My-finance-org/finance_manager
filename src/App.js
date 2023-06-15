1a991ce970e1fb99ede231011ac91bca3774e4d7
DarthVauban <dante2770@gmail.com>
2023-06-15 14:47:18 +0300

    FIN-11-clear-app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,3 @@
-import AddAccountsButton from './components/Buttons/AddAccountsButton';
-import CheckButton from './components/Buttons/CheckButton';
-import DetailsButton from './components/Buttons/DetailsButton';
-import GoogleSiginInButton from './components/Buttons/GoogleSigiInButton';
-import LoginButton from './components/Buttons/LoginButton';
 import style from './App.module.scss';
 
 function App() {
@@ -17,11 +12,6 @@ function App() {
         <p>
           FINANCE MANAGER 
         </p>
-        <LoginButton text="Login" onClick={handler}/>
-        <GoogleSiginInButton text="Continue with Google" onClick={handler}/>
-        <DetailsButton text="Details" onClick={handler}/>
-        <AddAccountsButton text="Add accounts" onClick={handler}/>
-        <CheckButton text="Check" onClick={handler}/>
       </header>
     </div>
   );
