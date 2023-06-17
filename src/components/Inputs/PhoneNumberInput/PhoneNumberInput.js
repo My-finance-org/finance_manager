f68d4e599126d88bd5591ee1c0d07e0e3ef6050c
DarthVauban <dante2770@gmail.com>
2023-06-17 22:50:29 +0300

    create phone number input FIN-12

---
+++ src/components/Inputs/PhoneNumberInput/PhoneNumberInput.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect, } from 'react';
+import PropTypes from 'prop-types';
+import PhoneInput from 'react-phone-input-2'
+// import 'react-phone-input-2/lib/style.css'
+import 'react-phone-input-2/lib/bootstrap.css'
+import styles from './PhoneNumberInput.module.scss'
+
+const PhoneNumberInput = ({ vlaue, onChange }) => {
+  return (
+    <PhoneInput
+        inputProps={{
+          name: 'phone',
+          required: true,
+          autoFocus: true,
+          enableSearch: true,
+        }}
+        containerClass={styles.inputContaier}
+        inputClass={styles.input}
+        dropdownClass={styles.dropdown}
+      />
+  );
+};
+
+PhoneNumberInput.propTypes = {
+  value: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default PhoneNumberInput;
