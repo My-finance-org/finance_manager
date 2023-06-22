f8bf785f50eb94e6b106f77d096f9bd01a2b9fd2
DarthVauban <dante2770@gmail.com>
2023-06-22 22:02:07 +0300

    fixed search input FIN-55

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -2,24 +2,24 @@ import * as React from "react";
 import SideBarLinksList from "./components/SideBarLinks/SideBarLinksList";
 import LogOutButton from "./components/LogOut";
 import ProfileLink from "./components/ProfileLink/ProfileLink";
-import userPhoto from '@/assets/Icons/ProfileImage.svg'
+import userPhoto from "@/assets/Icons/ProfileImage.svg";
 import styles from "./SideBar.module.scss";
 
 export default function SideBar() {
   return (
     <div className={styles.SideBar}>
       <div className={styles.sideBarLinks}>
-      <p className={styles.headerTitle}>
-        <span className={styles.boldText}>Finance</span> Manager.{" "}
-        <span className={styles.boldText}>IO</span>
-      </p>  
+        <p className={styles.headerTitle}>
+          <span className={styles.boldText}>Finance</span> Manager.{" "}
+          <span className={styles.boldText}>IO</span>
+        </p>
         <SideBarLinksList />
       </div>
       <div className={styles.sideBarFooter}>
         <LogOutButton />
-        <ProfileLink 
-        userName={'Tanzir Rahman'}
-        userPhoto={userPhoto}
+        <ProfileLink
+          userName={"Tanzir Rahman"}
+          userPhoto={userPhoto}
         />
       </div>
     </div>
