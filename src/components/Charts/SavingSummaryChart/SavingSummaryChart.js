877c4b17c3ab9d043e026b8cc67d962a0ceb0a6b
Bohdan Volkov <bohdan.volkov@dxc.com>
2023-06-23 12:30:33 +0300

    small fix DEV

--- src/components/Charts/SavingSummaryChart/SavingSummaryChart.js
+++ src/components/Charts/SavingSummaryChart/SavingSummaryChart.js
@@ -11,6 +11,8 @@ import {
   linearGradient,
   stop,
 } from "recharts";
+import "./SavingSummaryChart.scss";
+import BaseTitle from "@/components/shared/BaseTitle";
 
 const data = [
   {
@@ -20,109 +22,126 @@ const data = [
     amt: 2400,
   },
   {
-    name: "May 01",
+    name: "May 05",
     lastManth: 3000,
     thisManth: 4400,
     amt: 2400,
   },
   {
-    name: "May 01",
+    name: "May 10",
     lastManth: 1000,
     thisManth: 5400,
     amt: 2400,
   },
   {
-    name: "May 01",
+    name: "May 15",
     lastManth: 2000,
     thisManth: 3400,
     amt: 2400,
   },
   {
-    name: "May 01",
+    name: "May 20",
     lastManth: 4400,
     thisManth: 1400,
     amt: 2400,
   },
   {
-    name: "May 01",
+    name: "May 25",
     lastManth: 2060,
     thisManth: 2400,
     amt: 2400,
   },
   {
-    name: "May 01",
+    name: "May 30",
     lastManth: 1000,
-    thisManth: 2400,
-    amt: 2400,
-  },
-  {
-    name: "May 01",
-    lastManth: 1500,
-    thisManth: 3400,
-    amt: 2400,
-  },
-  {
-    name: "May 01",
-    lastManth: 4000,
     thisManth: 2400,
     amt: 2400,
   },
 ];
 
 const SavingSummaryChart = () => {
+  const formatYAxisValue = value => {
+    return `$${value}`;
+  };
+
   return (
-    <AreaChart
-      width={712}
-      height={229}
-      data={data}
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0,
-      }}
-    >
-      <CartesianGrid
-        horizontal={false}
-        strokeDasharray="3 3"
+    <div className="savingSummaryChart">
+      <BaseTitle
+        text="Saving Summary"
+        fontSize="16"
+        classes="savingSummaryChart__title"
       />
-      <YAxis axisLine={false} />
-      <Tooltip />
-      <defs>
-        <linearGradient
-          id="colorUv"
-          x1="0"
-          y1="0"
-          x2="0"
-          y2="1"
-        >
-          <stop
-            offset="20%"
-            stopColor="rgba(41, 157, 145, 0.5)"
-            stopOpacity={0.8}
+      <AreaChart
+        width={670}
+        height={180}
+        data={data}
+        margin={{
+          top: 10,
+          right: 30,
+          left: 0,
+          bottom: 0,
+        }}
+      >
+        <CartesianGrid
+          horizontal={false}
+          strokeDasharray="3 3"
+        />
+        <XAxis
+          dataKey="name"
+          axisLine={false}
+          tickLine={{ display: "none" }}
+          tick={{ fill: "#9F9F9F" }}
+          tickMargin={10}
+        />
+        <YAxis
+          axisLine={false}
+          tick={{ fill: "#9F9F9F" }}
+          tickLine={{ display: "none" }}
+          tickFormatter={formatYAxisValue}
+          tickMargin={5}
+        />
+        <Tooltip />
+        <defs>
+          <linearGradient
+            id="colorUv"
+            x1="0"
+            y1="0"
+            x2="0"
+            y2="1"
+          >
+            <stop
+              offset="30%"
+              stopColor="rgba(41, 157, 145, 0.5)"
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="70%"
+              stopColor="rgba(217, 217, 217, 0)"
+              stopOpacity={0}
+            />
+          </linearGradient>
+        </defs>
+        <Area
+          type="monotone"
+          dataKey="thisManth"
+          stroke="rgba(41, 157, 145, 0.5)"
+          strokeWidth={2}
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+        {true && (
+          <Area
+            type="monotone"
+            dataKey="lastManth"
+            stroke="rgba(209, 209, 209, 1)"
+            strokeWidth={2}
+            strokeDasharray="6 2"
+            fillOpacity={1}
+            fill="transparent"
           />
-          <stop
-            offset="80%"
-            stopColor="rgba(217, 217, 217, 0)"
-            stopOpacity={0}
-          />
-        </linearGradient>
-      </defs>
-      <Area
-        type="monotone"
-        dataKey="lastManth"
-        stroke="rgba(41, 157, 145, 0.5)"
-        fillOpacity={1}
-        fill="url(#colorUv)"
-      />
-      <Area
-        type="monotone"
-        dataKey="thisManth"
-        stroke="rgba(209, 209, 209, 1)"
-        fillOpacity={1}
-        fill="transparent"
-      />
-    </AreaChart>
+        )}
+      </AreaChart>
+    </div>
   );
 };
 
