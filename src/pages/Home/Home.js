586fa062720ce5bb0a44ac1253472a023b9a5b78
DarthVauban <dante2770@gmail.com>
2023-06-26 01:12:09 +0300

    fix imports FIN-24

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,10 +2,8 @@ import React from "react";
 import { MainLayout } from "../../layouts/MainLayout/MainLayout";
 import RecentTransaction from "../../components/RecentTransaction/";
 import VerticalBarChart from "@/components/Charts/VerticalBarChart";
-import Header from "@/components/Header";
 import ExpensesBreakdown from "@/components/ExpensesBreakdown";
 import styles from "./Home.module.scss";
-import GoalsCard from "@/components/Goals";
 
 export const Home = () => {
   return (
