f2c822c6cb53f44b251d729862b1864f5ae2301e
Bohdan Volkov <bohdan.volkov@dxc.com>
2023-06-27 14:37:39 +0300

    small fix FIN-64

--- src/pages/Goals/Goals.js
+++ src/pages/Goals/Goals.js
@@ -1,13 +1,19 @@
 import React from "react";
+import classNames from "classnames";
 import { MainLayout } from "@/layouts/MainLayout/MainLayout";
 import SavingSummaryChart from "@/components/Charts/SavingSummaryChart";
 import GoalsCard from "@/components/Goals";
+import "./Goals.scss";
 
 export const Goals = () => {
+  const clss = classNames("goals", {}, "flex");
+
   return (
     <MainLayout>
-      <SavingSummaryChart />
-      <GoalsCard />
+      <div className={clss}>
+        <GoalsCard />
+        <SavingSummaryChart />
+      </div>
     </MainLayout>
   );
 };
